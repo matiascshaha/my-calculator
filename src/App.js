2e07f46ca1a034eb8e048a0524fbbd34e5c9d9ed
Perichon <MPERICHO@ford.com>
2022-05-09 12:35:34 -0400

    initialize

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import myTitle from './components/MyTitle'
+
+const title = 'Matias Perichon Calculator'
+function App() {
+  return (
+    <div className="App">
+      <myTitle title={title}/>
+    </div>
+  );
+}
+
+
+
+export default App;
---
+++ src/components/MyTitle.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MyTitle = ({tet}) => {
+  return (
+    <div>
+        <h3>{tet}</h3>
+    </div>
+  )
+}
+
+export default MyTitle
