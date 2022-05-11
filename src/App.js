94e811c9a4f17e44f1a2348216850cbf3c4730b7
Perichon <MPERICHO@ford.com>
2022-05-11 12:20:42 -0400

    implemented some calculator logic

--- src/App.js
+++ src/App.js
@@ -13,18 +13,48 @@ function Calculator() {
     question: 'default',
     answer: 'default2'
   })
+  
+  //on click events
+  const handleOnClickEvents = (event) => {
+    const value = event.target.innerHTML
+    switch(value){
+        case '=': {
+            setOutputScreen({answer: 'math error'})
+            setOutputScreen({question: 'math error'})
+            break
+        }
+        case 'Clear': {
+            setOutputScreen({question: ''})
+            setOutputScreen({answer: ''})
+            break
+        }
+        case 'Delete': {
+            var ques = outputScreenState.question
+            ques = ques.slice(0, -1)
+            setOutputScreen({question: {ques}})
+            break
+        }
+        default: {
+            var ques2 = outputScreenState.question
+            ques2 += value
+            setOutputScreen({question: ques2})
+            break
+        }
+
+    }
+  }
 
   return (
     <div className="App">
       <Title title={title}/>
       <OutputScreen state={outputScreenState}/>
       <div>
-        <Button text='Clear' state={outputScreenState} setState={setOutputScreen}></Button>
-        <Button text='Delete' state={outputScreenState} setState={setOutputScreen}></Button>
-        <Button text='' state={outputScreenState} setState={setOutputScreen}></Button>
-        <Button text='/' state={outputScreenState} setState={setOutputScreen}></Button>
+        <Button text='Clear' hadnler={handleOnClickEvents}></Button>
+        <Button text='Delete' handler={handleOnClickEvents}></Button>
+        <Button text='' handler={handleOnClickEvents}></Button>
+        <Button text='/' handler={handleOnClickEvents}></Button>
       </div>
-      <div>
+      {/* <div>
         <Button text='7' state={outputScreenState} setState={setOutputScreen}></Button>
         <Button text='8' state={outputScreenState} setState={setOutputScreen}></Button>
         <Button text='9' state={outputScreenState} setState={setOutputScreen}></Button>
@@ -45,7 +75,7 @@ function Calculator() {
       <div>
         <Button text='0' state={outputScreenState} setState={setOutputScreen}></Button>
         <Button text='=' state={outputScreenState} setState={setOutputScreen}></Button>
-      </div> 
+      </div>  */}
     </div>
   );
 }
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,34 +1,9 @@
 import PropTypes from 'prop-types'
 
-const Button = ({text,state,setState}) => {
-
-    //on click events
-    const renderOnClickEvents = (text,state,setState) => {
-        switch(text){
-            case '=': {
-                setState({answer: 'math error'})
-                setState({question: 'math error'})
-                break
-            }
-            case 'Clear': {
-                setState({question: 'ff'})
-                break
-            }
-            case 'Delete': {
-                setState({question: 'ff'})
-                break
-            }
-            default: {
-                
-                break
-            }
-
-        }
-    }
-
+const Button = ({text,handler}) => {
     return (
         <div className="Button">
-            <button type='button' onClick={renderOnClickEvents({text}, {state}, {setState})}>{text}</button>
+            <button type='button' onClick={handler}>{text}</button>
         </div>
     )
 }
